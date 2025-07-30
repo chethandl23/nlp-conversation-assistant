17f758d46c7b61d9cd4909c3e477e6e809ee8de0
ChethanDL <Chethan.22ai010@sode-edu.in>
2025-07-31 00:08:28 +0530

    added multi question Q&A support with JSON logging

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from src.transcribe import transcribe_audio
 from src.summarize import summarize_text
-from src.questions_n_ans import ques_n_ans
+from src.questions_n_ans import ask_question
 audio_path = r"C:\Users\DELL\OneDrive\Documents\nlp-conversation-assistant\data\audio.mp3"
 
 # Call the transcribe function
@@ -25,22 +25,4 @@ with open("outputs/summary.txt","w",encoding="utf-8") as f:
     f.write(summary)
 print("summary saved..")
 
-with open("outputs/summary.txt","r",encoding = "utf-8") as f:
-    context = f.read()
-
-question = input("ask any doubt:")
-answer = ques_n_ans(question,context)
-
-qa_json_path = Path("outputs/qa_history.json")
-if qa_json_path.exists():
-    with open(qa_json_path, "r", encoding="utf-8") as f:
-        qa_history = json.load(f)
-else:
-    qa_history = []
-
-qa_history.append({"question": question, "answer": answer})
-
-with open(qa_json_path, "w", encoding="utf-8") as f:
-    json.dump(qa_history, f, ensure_ascii=False, indent=4)
-
-print("question and answer saved ")
+ask_question(summary)
--- src/questions_n_ans.py
+++ src/questions_n_ans.py
@@ -1,9 +1,23 @@
 from transformers import pipeline
+import json
+import os
 
+qa_pipline = pipeline("question-answering",model = "distilbert-base-uncased-distilled-squad")
+def ask_question(context:str,qa_file_path:str = "outputs/qa_history.json"):
+    qa_pairs = []
+    if os.path.exists(qa_file_path):
+        with open(qa_file_path, "r",encoding="utf-8") as f:
+            qa_pairs = json.load(f)
+    print("\nAsk a question based on the context:\n")
 
-def ques_n_ans(question :str,context:str,model_name:str = "google/flan-t5-base") -> str:
-    qa_pipeline = pipeline("text2text-generation",model = model_name)
-    prompt = f"Context:{context}\nQuestion:{question}\nAnswer:"
+    while True:
+        question = input("Q: ")
+        if question.lower() in ["exit","quit"]:
+            break
+        answer = qa_pipline(question=question,context=context)["answer"]
+        print(f"A: {answer}\n")
 
-    response =qa_pipeline(prompt,max_length =150,do_sample = False)[0]["generated_text"]
-    return response.strip()
+        qa_pairs.append({"question":question,"answer":answer})
+    
+    with open(qa_file_path,"w",encoding="utf-8") as f:
+        json.dump(qa_pairs,f,indent=4,ensure_ascii = False)
